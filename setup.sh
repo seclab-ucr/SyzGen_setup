9494742ef4af5aa68ed73ad750c9b48ac65a5a77
CvvT <weitengchencc@gmail.com>
2021-05-23 18:51:44 -0700

    install custom angr

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ set -e
 
 source common.sh
 
+pip3 install virtualenv
+brew install ldid
+brew install clang-format
+
 virtualenv ${SYZGEN} --python=$(which python3)
 
 # install golang
@@ -18,4 +22,15 @@ echo "export GOROOT" >> $VIRTUAL_ENV
 echo "GOPATH=\"${GOPATH}\"" >> $VIRTUAL_ENV
 echo "export GOPATH" >> $VIRTUAL_ENV
 
+git clone git@github.com:CvvT/SyzGen.git
+mkdir ${GOPATH}
+git clone git@github.com:CvvT/bluetooth-fuzzer.git ${GOPATH}/src/github.com/google/syzkaller
+
+# install custom angr
+git clone git@github.com:angr/angr.git
+cp angr.patch angr/
+cd angr
+git checkout -b dev ce14b4dd70f64
+git apply angr.patch
+rm angr.patch
 
